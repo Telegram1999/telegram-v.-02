549b5364f3d2ec8555339268fdf5ca525aa2c99d
Telegram1999 <106989715+Telegram1999@users.noreply.github.com>
2022-06-07 19:20:07 +0530

    Update __init__.py

--- DonLee_Robot/__init__.py
+++ DonLee_Robot/__init__.py
@@ -1,10 +1,10 @@
 # !/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # (c) @AlbertEinsteinTG
-# (e) @Muhammed_RK, @Mo_Tech_YT , @Mo_Tech_Group, @MT_Botz
+# (e) @KL_2335
 # Copyright permission under MIT License
 # All rights reserved by PR0FESS0R-99
-# License -> https://github.com/PR0FESS0R-99/DonLee_Robot/blob/main/LICENSE
+# License -> https://github.com/Telegram1999/DonLee_Robot/blob/main/LICENSE
 
 import os
 import logging
@@ -15,15 +15,15 @@ from logging.handlers import RotatingFileHandler
 from .Translation import Translation
 
 # Change Accordingly While Deploying To A VPS
-# API_ID From https://youtu.be/5eEsvLAKVc0
+# API_ID From https://t.me/kmtz_channel_v3
 APP_ID = int(os.environ.get("APP_ID"))
-# API_HASH From https://youtu.be/5eEsvLAKVc0
+# API_HASH From https://t.me/kmtz_channel_v3
 API_HASH = os.environ.get("API_HASH")
-# BOT_TOKEN From https://youtu.be/cB4UduCcNWs
+# BOT_TOKEN From https://t.me/kmtz_channel_v3
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
-# DD_URI From https://youtu.be/cB4UduCcNWs
+# DD_URI From https://t.me/kmtz_channel_v3
 DB_URI = os.environ.get("DB_URI")
-# USER_SESSION From https://youtu.be/WUN_12-dYOM
+# USER_SESSION From https://t.me/kmtz_channel_v3
 USER_SESSION = os.environ.get("USER_SESSION")
 
 VERIFY = {}
